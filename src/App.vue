a1483a909d387446fd944886e005fecdd68bd295
baiyi <baiyi@tsign.cn>
2021-07-27 18:30:27 +0800

    格式化代码

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,10 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-// import HelloWorld from './components/HelloWorld.vue'
 
 export default defineComponent({
   name: 'App',
-  components: {
-    // HelloWorld
-  },
+  components: {},
 })
 </script>
 
